5fdbe135c38aba6528d335aad3bc02348ec53cec
Emma <eredfoot@gmail.com>
2018-07-03 18:38:37 -0600

    Finished RAVEN work Fuel Cycles

---
+++ FuelCycles/FC_divide.py
@@ -0,0 +1,1 @@
+#I want to take the sample dump information, take the 2nd column, subtract the 2nd column from 2200, and save that in a spreadsheet
--- FuelCycles/seasons/convert_mult_hist.py
+++ FuelCycles/seasons/convert_mult_hist.py
@@ -12,19 +12,19 @@ def multipleHistories(filename, season):
             # last line
             demand.append([float(lin.strip().split(",")[1]) for lin in lines[start_counter:cnt+1] if len(lin.strip()) > 0] )
             hours.append([int(lin.strip().split(",")[0]) for lin in lines[start_counter:cnt+1] if len(lin.strip()) > 0] )
-            print("here")
+            #print(hours)
 
     for hist_cnt in range(len(demand)):
       f_obj = open(season+"/"+season+str(hist_cnt)+".csv","w")
       f_obj.write(header.rstrip().strip()+"\n")
-      for ts in reversed(range(len(hours[hist_cnt]))):
+      for ts in range(len(hours[hist_cnt])):
         f_obj.write(str(hours[hist_cnt][ts])+","+str(demand[hist_cnt][ts])+"\n")
       f_obj.close()
 
     InputFile=open(season+"/raw_data_"+season+".csv","w")
     InputFile.write("scaling,filename"+"\n")
     for hist_cnt in range(len(demand)):
-        InputFile.write("1"+","+season+"_"+str(hist_cnt)+".csv"+"\n")
+        InputFile.write("1"+","+season+str(hist_cnt)+".csv"+"\n")
     InputFile.close()
 
 
